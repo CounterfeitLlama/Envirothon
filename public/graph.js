e993af7d22db187d8673e736e7ee65dc6ee7bfae
Andrew Turley <aturley123@gmail.com>
2018-04-21 19:52:04 -0400

    Removed chart aspect ratio

--- public/graph.js
+++ public/graph.js
@@ -23,42 +23,44 @@ function createGraph(dataList, dates) {
     // console.log(dataList[0])
     var ctx = document.getElementById("myChart");
     var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-       labels: dates,
-       datasets: [{
-           fill: false,
-           label: 'Metric tons of C02',
-           data: dataList, //use data input from firebase here, plug in some sort of measurement system
-           backgroundColor: [
-               'rgba(255, 99, 132, 0.2)',
-               'rgba(54, 162, 235, 0.2)',
-               'rgba(255, 206, 86, 0.2)',
-               'rgba(75, 192, 192, 0.2)',
-               'rgba(153, 102, 255, 0.2)',
-               'rgba(255, 159, 64, 0.2)'
-           ],
-           borderColor: [
-               'rgba(255,99,132,1)',
-               'rgba(54, 162, 235, 1)',
-               'rgba(255, 206, 86, 1)',
-               'rgba(75, 192, 192, 1)',
-               'rgba(153, 102, 255, 1)',
-               'rgba(255, 159, 64, 1)'
-           ],
-           borderWidth: 5
-       }]
-    },
-    options: {
-       scales: {
-           yAxes: [{
-               ticks: {
-                   beginAtZero: true
-               }
+        type: 'line',
+        data: {
+           labels: dates,
+           datasets: [{
+               fill: false,
+               label: 'Metric tons of C02',
+               data: dataList, //use data input from firebase here, plug in some sort of measurement system
+               backgroundColor: [
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(54, 162, 235, 0.2)',
+                   'rgba(255, 206, 86, 0.2)',
+                   'rgba(75, 192, 192, 0.2)',
+                   'rgba(153, 102, 255, 0.2)',
+                   'rgba(255, 159, 64, 0.2)'
+               ],
+               borderColor: [
+                   'rgba(255,99,132,1)',
+                   'rgba(54, 162, 235, 1)',
+                   'rgba(255, 206, 86, 1)',
+                   'rgba(75, 192, 192, 1)',
+                   'rgba(153, 102, 255, 1)',
+                   'rgba(255, 159, 64, 1)'
+               ],
+               borderWidth: 5
            }]
-       }
-    }
-   });
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                   ticks: {
+                       beginAtZero: true
+                   }
+                }]
+            },
+            responsive: true,
+            maintainAspectRatio: false
+        }
+    });
 }
 
 function dateToStr(date) {
