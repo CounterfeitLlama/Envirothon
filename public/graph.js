888e239e6b3f59bd152c0f5d0df2fcbd6a166ab4
Andrew Turley <aturley123@gmail.com>
2018-04-21 19:34:42 -0400

    Added dates to graph and fixed order

--- public/graph.js
+++ public/graph.js
@@ -1,88 +1,63 @@
 function initGraph(user) {
-		var numdays = 5;
-		var dates = [];
+	var numdays = 6;
+	var dates = [];
     var dataList = []
-		for(var i=0;i<5;i++) {
-			day = new Date();
-			day.setDate(day.getDate() - i);
-			dates.push(dateToStr(day));
-		}
+
+	for(var i=numdays-1;i>=0;i--) {
+		day = new Date();
+		day.setDate(day.getDate() - i);
+		dates.push(dateToStr(day));
+	}
 
     // var user = firebase.auth().currentUser;
     var uid = user.uid;
 
-		getDataDateRange(uid,dates,'total',dataList,function() {
-			createGraph(dataList);
-		});
-		/*
-    db.collection("users").doc(uid).collection(dateToStr(day5)).get().then(function(querySnapshot) {
-            querySnapshot.forEach(function(doc) {
-                console.log(doc.id, " => ", doc.get("total"));
-                dataList.push(doc.get("total"));
-            });
-        }).then(db.collection("users").doc(uid).collection(dateToStr(day4)).get().then(function(querySnapshot) {
-                querySnapshot.forEach(function(doc) {
-                    console.log(doc.id, " => ", doc.get("total"));
-                    dataList.push(doc.get("total"));
-                });
-        })).then(db.collection("users").doc(uid).collection(dateToStr(day3)).get().then(function(querySnapshot) {
-                querySnapshot.forEach(function(doc) {
-                    console.log(doc.id, " => ", doc.get("total"));
-                    dataList.push(doc.get("total"));
-                });
-        })).then(db.collection("users").doc(uid).collection(dateToStr(day2)).get().then(function(querySnapshot) {
-                querySnapshot.forEach(function(doc) {
-                    console.log(doc.id, " => ", doc.get("total"));
-                    dataList.push(doc.get("total"));
-                });
-        })).then(function() {
-            createGraph(dataList);
-        })
-				*/
+	getDataDateRange(uid,dates,'total',dataList,function() {
+		createGraph(dataList, dates);
+	});
 }
 
-function createGraph(dataList) {
-
+function createGraph(dataList, dates) {
     console.log("init graph")
-   console.log(dataList);
-   // console.log(dataList[0])
-   var ctx = document.getElementById("myChart");
-   var myChart = new Chart(ctx, {
-       type: 'line',
-       data: {
-           labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-           datasets: [{
-               fill: false,
-               label: 'Metric tons of C02',
-               data: dataList, //use data input from firebase here, plug in some sort of measurement system
-               backgroundColor: [
-                   'rgba(255, 99, 132, 0.2)',
-                   'rgba(54, 162, 235, 0.2)',
-                   'rgba(255, 206, 86, 0.2)',
-                   'rgba(75, 192, 192, 0.2)',
-                   'rgba(153, 102, 255, 0.2)',
-                   'rgba(255, 159, 64, 0.2)'
-               ],
-               borderColor: [
-                   'rgba(255,99,132,1)',
-                   'rgba(54, 162, 235, 1)',
-                   'rgba(255, 206, 86, 1)',
-                   'rgba(75, 192, 192, 1)',
-                   'rgba(153, 102, 255, 1)',
-                   'rgba(255, 159, 64, 1)'
-               ],
-               borderWidth: 5
+    console.log(dataList);
+    // console.log(dataList[0])
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+       labels: dates,
+       datasets: [{
+           fill: false,
+           label: 'Metric tons of C02',
+           data: dataList, //use data input from firebase here, plug in some sort of measurement system
+           backgroundColor: [
+               'rgba(255, 99, 132, 0.2)',
+               'rgba(54, 162, 235, 0.2)',
+               'rgba(255, 206, 86, 0.2)',
+               'rgba(75, 192, 192, 0.2)',
+               'rgba(153, 102, 255, 0.2)',
+               'rgba(255, 159, 64, 0.2)'
+           ],
+           borderColor: [
+               'rgba(255,99,132,1)',
+               'rgba(54, 162, 235, 1)',
+               'rgba(255, 206, 86, 1)',
+               'rgba(75, 192, 192, 1)',
+               'rgba(153, 102, 255, 1)',
+               'rgba(255, 159, 64, 1)'
+           ],
+           borderWidth: 5
+       }]
+    },
+    options: {
+       scales: {
+           yAxes: [{
+               ticks: {
+                   beginAtZero: true
+               }
            }]
-       },
-       options: {
-           scales: {
-               yAxes: [{
-                   ticks: {
-                       beginAtZero: true
-                   }
-               }]
-           }
        }
+    }
    });
 }
 
