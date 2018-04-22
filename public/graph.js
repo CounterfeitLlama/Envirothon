c3403d9ae2f091784913e69efe2e1d240aba063a
Andrew Turley <aturley123@gmail.com>
2018-04-21 20:00:37 -0400

    Removed useless colors

--- public/graph.js
+++ public/graph.js
@@ -30,22 +30,8 @@ function createGraph(dataList, dates) {
                fill: false,
                label: 'Metric tons of C02',
                data: dataList, //use data input from firebase here, plug in some sort of measurement system
-               backgroundColor: [
-                   'rgba(255, 99, 132, 0.2)',
-                   'rgba(54, 162, 235, 0.2)',
-                   'rgba(255, 206, 86, 0.2)',
-                   'rgba(75, 192, 192, 0.2)',
-                   'rgba(153, 102, 255, 0.2)',
-                   'rgba(255, 159, 64, 0.2)'
-               ],
-               borderColor: [
-                   'rgba(255,99,132,1)',
-                   'rgba(54, 162, 235, 1)',
-                   'rgba(255, 206, 86, 1)',
-                   'rgba(75, 192, 192, 1)',
-                   'rgba(153, 102, 255, 1)',
-                   'rgba(255, 159, 64, 1)'
-               ],
+               backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+               borderColor: ['rgba(255,99,132,1)'],
                borderWidth: 5
            }]
         },
