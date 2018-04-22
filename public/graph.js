9070f9f06ed271fe0be1611524e4457c1496e613
Andrew Turley <aturley123@gmail.com>
2018-04-21 23:06:46 -0400

    Added graph to analyze.html

--- public/graph.js
+++ public/graph.js
@@ -1,40 +1,3 @@
-function initExampleGraph() {
-    var dates = [];
-
-    for(var i=6; i>=0; i--) {
-        day = new Date();
-        day.setDate(day.getDate() - i);
-        dates.push(dateToStr(day));
-    }
-
-    var ctx = document.getElementById("myChart");
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-           labels: dates,
-           datasets: [{
-               fill: false,
-               label: 'Metric tons of C02',
-               data: [10, 7, 17, 7, 8, 22, 13], //use data input from firebase here, plug in some sort of measurement system
-               backgroundColor: ['rgba(255, 99, 132, 0.2)'],
-               borderColor: ['rgba(255,99,132,1)'],
-               borderWidth: 3
-           }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                   ticks: {
-                       beginAtZero: true
-                   }
-                }]
-            },
-            responsive: true,
-            maintainAspectRatio: false
-        }
-    });
-}
-
 function initGraph(uid, numdays) {
 	var dates = [];
     var dataList = []
@@ -51,19 +14,17 @@ function initGraph(uid, numdays) {
 	});
 }
 
-function createGraph(dataList, dates) {
-    console.log(dates.length);
-    console.log(dataList.length);
+function createGraph(dataList, dates, id="myChart") {
     dates.reverse();
     dataList.reverse();
     if (dataList.length < dates.length) {
         dates = dates.slice(dates.length - dataList.length)
     }
-
+    
     console.log("init graph")
-    console.log(dataList);
+    console.log(id);
     // console.log(dataList[0])
-    var ctx = document.getElementById("myChart");
+    var ctx = document.getElementById(id);
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
